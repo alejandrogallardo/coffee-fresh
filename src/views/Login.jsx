52c336b05a8f50f685ed3079c0244c4e649baaa1
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2025-04-27 19:12:03 -0600

    Creacion de navegacion y paginas de auth

---
+++ src/views/Login.jsx
@@ -0,0 +1,40 @@
+import {Link} from "react-router-dom";
+
+export default function Login() {
+    return (
+        <>
+            <h1 className="text-4xl font-black">Iniciar Sesión</h1>
+            <p>Para realizar un pedido debes iniciar sesión</p>
+            <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+                <form>
+                    <div className="mb-4">
+                        <label className="text-slate-800" htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            placeholder="Tu email"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="text-slate-800" htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            placeholder="Tu password"
+                        />
+                    </div>
+                    <input
+                        type="submit"
+                        value="Iniciar Sesión"
+                        className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+                    />
+                </form>
+            </div>
+             <nav className="mt-5">
+                 <Link to="/auth/registro">Crea una cuenta</Link>
+             </nav>
+        </>
+    )
+}
